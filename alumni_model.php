2c4737a2e31c164964410d94bee19ffdd233186b
JRlibao <149857763+JRlibao@users.noreply.github.com>
2023-11-11 12:07:34 +0800

    Update alumni_model.php

--- alumni_model.php
+++ alumni_model.php
@@ -1,119 +1,53 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?= base_url('assets/css/bootstrap.css')?>">
-    <title>Student's information</title>
-    <style>
-        a
-        {
-            position: left;
-            margin-left: 5px;
-        }
-        h1
-        {
-            position: center;
-            margin-left: 450px;
-            font: bold;
-        }
-        .container
-        {
-            
-            position: center;
-            margin-left: 60px;
-        }
-        body
-        {
-            background-color: #9fb0c9;
-        }
-    </style>
-</head>
-<body>
+<?php
+class alumni_model extends CI_Model
+{
+    public function __construct()
+    {
+        parent:: __construct();
+        $this->table = 'alumni';
+    }
 
-<h1>STUDENT'S INFORMATIONS</h1>
-<div class="container">
-<table class= "table bordered">
-        <thead >
-            <tr  >
-                <th >
-                   Last Name
-                </th>
-                <th >
-                   First Name 
-                </th>
-                <th >
-                   Middle Name
-                </th>
-                <th>
-                   Contacts
-                </th>
-                <th>
-                    Email
-                </th>
-                <th >
-                    Gender
-                </th>
-                <th >
-                  Degree
-                </th>
-                <th >
-                  Instructor
-                </th>
-                <th >
-                 Section
-                </th>
-                <th >
-                <a href="<?= site_url('students/add')?>"> <button>Create</button></a>
-                </th>
+    public function rows()
+    {
+        $where = array(
+            'is_Deleted' => 0
+        );
+        $query = $this->db->get_where($this->table, $where);
+        return $query->result();
 
-            
-            </tr>
-        </thead>
+    }
+    public function row($id)
+    {
+        $where = array(
+            'is_deleted' => 0,
+            'id' => $id
+        );
+        $query = $this->db->get_where($this->table, $where);
+        return $query->row();
+    }
 
-        <tbody>
-            <?php foreach($students as $student) : ?>
-            <tr >
-                <td>
-                   <?= $student-> Lastname ?>
-                </td>
-                <td>
-                <?= $student-> Firstname ?>
-                </td>
-                <td>
-                <?= $student-> Middlename ?>
-                </td>
-              
-                <td>
-                <?= $student-> contacts ?>
-                </td>
-                <td>
-                <?= $student-> email ?>
-                </td>
-                <td>
-                <?= $student-> gender ?>
-                </td>
-                <td>
-                <?= $student-> course_id ?>
-                </td>
-                <td>
-                <?= $student-> instructor_id ?>
-                </td>
-                <td>
-                <?= $student-> Section_id ?>
-                </td>
-                <td>
-                <a href="<?= site_url('Students/show/' . $student->id)?>"><button>Edit</button></a> |<a href="<?= site_url('Students/delete/' . $student->id)?>"><button>Delete</button></a>
-                </td>
-                
-            </tr>
-            <?php endforeach ?>
+    public function add($data)
+    {
+        $this->db->insert($this->table, $data);
+        return true;
+    }
 
+    public function edit($id, $data)
+    {
+        $where = array(
+            'is_deleted' => 0,
+            'id'         => $id
+        );
+        return $this->db->update($this->table,$data,$where);
+    }
 
-       </tbody>
-    </table>
+    public function delete($id, $data)
+    {
+        $where = array(
+            'is_Deleted' => 0,
+            'id'         => $id
+        );
+        return $this->db->update($this->table, $data, $where);
 
-    </div>
-
-</body>
-</html>
+    }
+}
